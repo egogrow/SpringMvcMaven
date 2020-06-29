d58e5eede033f8f2bef1d68b3f536d47d568c01e
lois <lois@lotte.net>
2020-06-29 17:42:24 +0900

    수정 테스트

--- src/main/java/com/smm/controller/WebController.java
+++ src/main/java/com/smm/controller/WebController.java
@@ -9,6 +9,7 @@ public class WebController {
     @RequestMapping("/home")
     public String web(){
         System.out.println("@@메[시지 출력");
+        System.out.println("수정");
         return"index";
     }
 
